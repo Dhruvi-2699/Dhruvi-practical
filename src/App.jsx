7c4a45f153d0f59122f73da7629b225035fa9185
Dhruvi <ramidhruvi26@gmail.com>
2025-02-01 13:41:11 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='max-w-4xl mx-auto'>
+      <div className='flex justify-center items-center'>
+        <h1 className="text-xl font-bold">Package List</h1>
+        
+      </div>
+
+    
+    </div>
+  )
+}
+
+export default App
