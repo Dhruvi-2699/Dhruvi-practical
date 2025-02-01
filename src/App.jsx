fb78f0d0ad98919ce6eb30331a550d6ddcac1603
Dhruvi <ramidhruvi26@gmail.com>
2025-02-01 15:56:23 +0530

    set up with chakra

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react'
 import './App.css'
+import AddPackageForm from './components/addPackageForm';
+import PopupComponent from './components/poupComponent';
 
 function App() {
+    const [openAddPackageForm, setOpenAddPackageForm] = useState(false);
 
+  const handleOnClose =()=>{
+    setOpenAddPackageForm(false)
+  }
   return (
-    <div className='max-w-4xl mx-auto'>
+    <div className='flex flex-row justify-center items-center h-screen'>
       <div className='flex justify-center items-center'>
         <h1 className="text-xl font-bold">Package List</h1>
-        
+        <button onClick={()=>setOpenAddPackageForm(true)}>Add new  Package</button>
       </div>
 
-    
+    {
+      openAddPackageForm &&
+      <PopupComponent isOpen={openAddPackageForm} onClose={handleOnClose}>
+        <AddPackageForm />
+        </PopupComponent>
+    }
     </div>
   )
 }
